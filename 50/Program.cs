71c6f6aaffa1c42e5c15b2bca30950b3652da874
Mishanya <mishastefanovich@mail.ru>
2023-06-30 15:14:53 +0300

    домашка

---
+++ 50/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows=int.Parse(ReadLine()!);
+
+Write("Введите количество столбцов массива: ");
+int columns=int.Parse(ReadLine()!);
+
+Write("Введите строку элемента: ");
+int strok =int.Parse(ReadLine()!);
+
+Write("Введите столбец элемента: ");
+int stolb =int.Parse(ReadLine()!);
+
+
+
+int[,] array = GetArray(rows, columns, 0, 10);
+
+
+PrintArray(array);
+
+WriteLine();
+
+FindElement(array,strok,stolb);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j]} ");
+        }
+        WriteLine();
+    }
+}
+
+void FindElement(int[,] inArray,int a,int b)
+{
+if (a>inArray.GetLength(0) || b>inArray.GetLength(1)) {
+    Write("такого числа в массиве нет");
+    
+}
+else Write (inArray[a-1,b-1]);
+}
---
+++ 52/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows=int.Parse(ReadLine()!);
+
+Write("Введите количество столбцов массива: ");
+int columns=int.Parse(ReadLine()!);
+
+
+
+int[,] array = GetArray(rows, columns, 0, 10);
+
+
+PrintArray(array);
+
+WriteLine();
+
+SredneeStolb(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j]} ");
+        }
+        WriteLine();
+    }
+}
+
+void SredneeStolb(int[,] inArray)
+{ Write("Среднее арифметическое каждого столбца:");
+    for (int i = 0; i < inArray.GetLength(1); i++)
+    {
+        double count =0;
+        double summ = 0;
+        for (int j = 0; j < inArray.GetLength(0); j++)
+        {
+            summ += inArray[j,i];
+           count++;
+        }
+        Write ($" {summ/count:f1}");
+        
+    }
+
+}
